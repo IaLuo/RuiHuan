2baaafc87c67dac366ae003094842ac1fa87c56c
夏落 <xialuo@hugangtekimacbook-pro.local>
2016-04-21 15:10:03 +0800

    你好  －夏

--- PracticeDemo/PracticeDemo/ViewController.swift
+++ PracticeDemo/PracticeDemo/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print("大家好")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
